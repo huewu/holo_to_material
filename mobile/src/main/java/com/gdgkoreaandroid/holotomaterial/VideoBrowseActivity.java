72931d8e730dbd29144677a1ed70b7c717d508c5
Chansuk Yang <chansuk@google.com>
2015-03-17 12:43:29 +0900

    use material style navigation drawer

--- mobile/src/main/java/com/gdgkoreaandroid/holotomaterial/VideoBrowseActivity.java
+++ mobile/src/main/java/com/gdgkoreaandroid/holotomaterial/VideoBrowseActivity.java
@@ -5,7 +5,8 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
+//import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -98,7 +99,7 @@ public class VideoBrowseActivity extends ActionBarActivity
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,
                 mDrawerLayout,
-                R.drawable.ic_drawer,
+                mToolbar,
                 R.string.drawer_open,
                 R.string.drawer_close
         ) {
